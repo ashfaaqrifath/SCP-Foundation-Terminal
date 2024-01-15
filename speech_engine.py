42a04b7b7242d6b67d94bc2b3bb1c98ed5a355cf
Ashfaaq Rifath <ashfaaq.rifath2@gmail.com>
2024-01-15 10:28:35 +0530

    Delete speech_engine.py

--- speech_engine.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-
-def speak(talk):
-    engine = pyttsx3.init("sapi5")
-    engine.setProperty('rate', 150)
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[1].id)
-    engine.say(talk)
-    engine.runAndWait()
